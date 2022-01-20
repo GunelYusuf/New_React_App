f20fd0550464044093fb9ea1ffd995e32b1e7af7
GunelYusuf <91114321+GunelYusuf@users.noreply.github.com>
2022-01-20 21:06:29 +0400

    fixed state and setState for Counter component

--- src/Counter.js
+++ src/Counter.js
@@ -8,19 +8,48 @@ const buttonStyle = {
 export const Counter = () =>{
 
     const [counter,setCounter] = React.useState(1);
+    
+    const handleButtonClick=(type='INCREASE')=>{
+        switch(type){
+            case 'INCREASE':{
+                setCounter((oldCounter)=>  oldCounter + 1);
+                break;
+            }
+            case 'DECREASE':{
+                 setCounter((oldCounter)=> oldCounter>1 ? oldCounter - 1 : oldCounter);
+                 break;
+            }
+            case 'RESET':{
+                 setCounter(1);
+                 break;
+            }
+            default:
+                 break;
+        }
+    }
 
-    const handleDecreaseClick=()=>{
-      setCounter((oldCounter)=> oldCounter>1 ? oldCounter - 1 : oldCounter)
-             
-    }
-    const handleIncreaseClick=()=>{
-        setCounter((oldCounter)=>  oldCounter + 1)
-    }
     return (
         <div>
             <h4>{counter}</h4>
-            <button style={buttonStyle} onClick={handleDecreaseClick}>-</button>
-            <button style={buttonStyle} onClick={handleIncreaseClick}>+</button>
+            <button 
+            style={buttonStyle} 
+            onClick={()=>{handleButtonClick('DECREASE')}}
+            > 
+                -
+            </button>
+
+            <button 
+            style={buttonStyle} 
+            onClick={()=>{handleButtonClick()}}
+            >
+                +
+            </button>
+
+            <button style={buttonStyle}
+             onClick={()=>{handleButtonClick('RESET')}}
+             > 
+               Reset
+            </button>
         </div>
     )
 }
